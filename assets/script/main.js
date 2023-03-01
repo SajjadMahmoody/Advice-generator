753d3161d2e86c38c26d44933229f9f5fda8d627
sajjad Mahmoody <maxmahmoody@gmail.com>
2023-03-01 22:31:17 +0330

    show id

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,12 +1,14 @@
 let btn = document.querySelector("#btn");
-let msg = document.querySelector("#show-msg");
+let showmsg = document.querySelector("#show-msg");
+let showid = document.querySelector("#show-id");
 async function doFetch() {
   try {
     const response = await fetch("https://api.adviceslip.com/advice", {
       method: "GET",
     });
     const data = await response.json();
-    msg.innerHTML = `" ${data.slip.advice}`;
+    showmsg.innerHTML = `" ${data.slip.advice} "`;
+    showid.innerHTML = `#${data.slip.id}`;
   } catch (error) {
     console.error(error);
   }
